acd41a316f5e5703857ef4b6cd2618fc1dce60f1
Gustavo <gustavoschimidt23@gmail.com>
2021-09-04 17:35:38 -0300

    aulas python

---
+++ aula2/a2-4-1.py
@@ -0,0 +1,14 @@
+# Curso: Python Fundamentos Proway
+# Aula : Dia 2 - Parte 4
+# Assunto: for com listas e tuplas
+# Data: 2021-09-204
+
+nomes = ['Ana', 'Paula', 'Matheus']
+
+for n in nomes:
+    print(n)
+
+    numeros_sorteados = (5,6,7,4,3,4,13)
+    
+    for ns in numeros_sorteados:
+        print(ns) 
