21c117142cb95772839a0147752f4e978fb451ac
Gustavo <gustavoschimidt23@gmail.com>
2021-09-04 17:51:33 -0300

    aulas python

--- aula2/a2-4-1.py
+++ aula2/a2-4-1.py
@@ -4,11 +4,11 @@
 # Data: 2021-09-204
 
 nomes = ['Ana', 'Paula', 'Matheus']
-
 for n in nomes:
     print(n)
 
     numeros_sorteados = (5,6,7,4,3,4,13)
-    
     for ns in numeros_sorteados:
         print(ns) 
+
+        
