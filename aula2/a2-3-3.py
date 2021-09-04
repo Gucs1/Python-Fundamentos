62c6e74a652a68077e2979033c588efaa75530bd
Gustavo <gustavoschimidt23@gmail.com>
2021-09-04 14:43:33 -0300

    aulas python2

--- aula2/a2-3-3.py
+++ aula2/a2-3-3.py
@@ -5,13 +5,38 @@
 
 #bebidas = 
 
-lista = ['gustavo']
+lista = ['listagem1']
 tuplas = ('gustavo')
-dicionario = {'nome':'Gustavo', 'sobrenome':'Coelho', 'idade':10}
 
 lista[0]
 tuplas[0]
 
+# criando um dicionario
+dicionario = {'nome':'Gustavo', 'sobrenome':'Coelho', 'idade':10,}
+
+# lendo um dicionario
 print(dicionario['nome'])
 print(dicionario['sobrenome'])
 print(dicionario['idade'])
+
+
+# alternando dados de um dicionario
+dicionario['nome'] = 'Tamires'
+dicionario['sobrenome'] = 'Azeredo'
+dicionario['idade'] = 17
+
+# lendo dados alterados
+print(dicionario['nome'])
+print(dicionario['sobrenome'])
+print(dicionario['idade'])
+
+# adicionando um novo conjunto d chave;valor
+dicionario['cpf'] = '5912390817'
+print(dicionario['cpf'])
+
+clientes = {}
+clientes2 = {'nome':'', 'sobrenome':'', 'idade':19}
+
+clientes['nome']= input('Digite seu nome: ')
+clientes['sobrenome']= input('Digite o sobrenome: ')
+clientes['nome']= int(input('Digite a idade: '))
