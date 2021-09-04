5605e7b9221bcc21f50ff2b61531289466015459
Gustavo <gustavoschimidt23@gmail.com>
2021-09-04 14:18:07 -0300

    aulas python

---
+++ aula2/a2-3-3.py
@@ -0,0 +1,17 @@
+# Curso: Python Fundamentos Proway
+# Aula : Dia 2 - Parte 3
+# Assunto: Dicionários
+# Data: 2021-08-28
+
+#bebidas = 
+
+lista = ['gustavo']
+tuplas = ('gustavo')
+dicionario = {'nome':'Gustavo', 'sobrenome':'Coelho', 'idade':10}
+
+lista[0]
+tuplas[0]
+
+print(dicionario['nome'])
+print(dicionario['sobrenome'])
+print(dicionario['idade'])
