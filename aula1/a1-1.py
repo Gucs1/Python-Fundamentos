f083fffac8b1879f0fffdf62a86d786fdbd570af
Gucs1 <Gustavoschimidt23@gmal.com>
2021-09-11 14:01:30 -0300

    Aulas python

--- aula1/a1-1.py
+++
@@ -1,5 +0,0 @@
-print('Olá, tchurma')
-print("Cachaça")
-print('Olá', 'Vanderlei', 'Diter', 'Gabriel', 'Felipe')
-print('Olá', '\nVanderlei', '\nDiter', '\nGabriel', '\nFelipe')
-print('Olá', '\n\tVanderlei', '\n\tDiter', '\n\tGabriel', '\n\tFelipe')
