b2e2391c9c7ffd65b70d7abe0813128897ea5144
Guscs1 <Gustavoschimidt23@gmail.com>
2021-09-11 17:11:46 -0300

    aula4

---
+++ aula4/a4-1.py
@@ -0,0 +1,8 @@
+# Curso: Python Fundamentos Proway
+# Aula : Dia 3 - Parte 4 - 1
+# Assunto: Arquivos
+# Data: 2021-09-11
+
+arquivo = open('aula4/cliente.txt' , 'w')
+arquivo.write ('cliente1-ClientePF')
+arquivo.close()
