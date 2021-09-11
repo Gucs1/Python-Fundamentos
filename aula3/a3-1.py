cc3160966b929f98ebf2ef0879787e725be3c139
Guscs1 <Gustavoschimidt23@gmail.com>
2021-09-11 14:24:00 -0300

    Aulas python new

--- aula3/a3-1.py
+++ aula3/a3-1.py
@@ -3,3 +3,30 @@
 # Assunto:funcoes
 # Data: 2021-09-11
 
+# Função = Existe retorno
+# Procedimento = não possui retorno
+# Parametro = É a variavel recebida em uma funcao ou procedimento
+# Argumento = É o dado passado durante a chamada de uma funcao ou procedimento
+
+#somar
+def somar(numero1, numero2):
+    resultado = numero1 + numero2
+    print(resultado)
+
+#subtrair
+def sub(numero1, numero2):
+    resultado = numero1 - numero2
+    print(resultado)
+
+#multiplicar
+def mult(numero1, numero2):
+    resultado = numero1 * numero2
+    print(resultado)
+
+#dividir
+def div(numero1, numero2):
+    resultado = numero1 / numero2
+    print(resultado)
+
+somar(5 + 5)
+
